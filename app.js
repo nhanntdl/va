4a6554858ac45b2069572c3247c54d85a035b521
Nguyễn Trung Nhân <nhanntdl98@gmail.com>
2020-09-11 22:48:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
+const ownerName = 'Nhan';
+
+const greetings = [
+  "Im good you little of love",
+  "Doing good homeboi",
+  "leav me alone",
+];
+
+const speechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new speechRecognition();
+
+recognition.onstart = function () {
+  console.log("voice is activeted");
+};
+
+recognition.onresult = function (event) {
+  const current = event.resultIndex;
+
+  const transcript = event.results[current][0].transcript;
+  content.textContent = transcript;
+  readOutLoud(transcript);
+};
+
+btn.addEventListener("click", () => {
+  recognition.start();
+});
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = "i dont know what you said";
+  if (message.includes("How are you")) {
+    const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+    speech.text = finalText;
+  }
+
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
+
+function sayHi() {
+  const speech = new SpeechSynthesisUtterance();
+  const hellotext = 'Hello, welcome back! ' + ownerName;
+  speech.text = hellotext;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  window.speechSynthesis.speak(speech);
+  content.textContent = hellotext;
+}
+
+sayHi();
